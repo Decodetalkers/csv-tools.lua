16e5796483b4df670111d34ac57ab0ee1998e69d
cht <3237126351@qq.com>
2022-02-16 14:47:15 +0800

    use up and down to highlight

--- ftdetect/csv-tools.vim
+++ ftdetect/csv-tools.vim
@@ -1,2 +1,3 @@
+au BufRead,BufNewFile *.csv,*.dat,*.tsv,*.tab set filetype=csv
 au FileType csv command! -buffer CloseTopWindow lua require"csvtools".CloseWindow()
 au FileType csv command! -buffer TopWindow lua require"csvtools".NewWindow()
--- lua/csvtools.lua
+++ lua/csvtools.lua
@@ -4,18 +4,20 @@ local M = {
 	winid = nil,
 	buf = nil,
 	mainwindowbuf = nil,
+	before = 10,
+	after = 10,
 }
+local highlight = require("csvtools.topbarhighlight")
 function M.NewWindow()
 	if M.winid == nil then
 		M.mainwindowbuf = vim.api.nvim_get_current_buf()
-		local file = vim.api.nvim_buf_get_name(0)
-		local f = io.open(file, "r")
-
-		local messages = f:read()
+		--local file = vim.api.nvim_buf_get_name(0)
+		--local f = io.open(file, "r")
+		local messages = unpack(api.nvim_buf_get_lines(M.mainwindowbuf,0,1,true))
 		if messages == nil then
 			return
 		end
-		f:close()
+		--f:close()
 		messages = messages:gsub("%,", "|")
 		local buf = api.nvim_create_buf(false, true) -- create new emtpy buffer
 		vim.cmd([[sview]])
@@ -23,7 +25,7 @@ function M.NewWindow()
 		local win = vim.api.nvim_get_current_win()
 		api.nvim_buf_set_lines(buf, 0, -1, false, { messages })
 		api.nvim_win_set_buf(win, buf)
-		require("csvtools.topbarhighlight").hightlight(buf, messages)
+		highlight.highlighttop(buf, messages)
 		--api.nvim_buf_add_highlight(buf, -1, 'WhidHeader',0,0,1)
 		--api.nvim_buf_add_highlight(buf, -1, 'WhidSubHeader', 0, 1, 2)
 		M.winid = win
@@ -38,6 +40,34 @@ function M.CloseWindow()
 		M.buf = nil
 	end
 end
+local function getrange (line,length)
+	local start = 1
+	if line - M.before > 1 then
+		start = line - M.before
+	end
+	local final = length
+	if line + M.after < length then
+		final = line + M.after
+	end
+	return start, final
+end
+function M.Highlight()
+	if vim.o.filetype == "csv" then
+
+		M.mainwindowbuf = vim.api.nvim_get_current_buf()
+		local line ,_ = unpack(vim.api.nvim_win_get_cursor(0))
+		local length = vim.api.nvim_buf_line_count(M.mainwindowbuf)
+		local start, final  = getrange(line,length)
+		--print(start)
+		--print(final)
+		for i = start, line, 1 do
+			highlight.highlight(M.mainwindowbuf,i)
+		end
+		for i = line, final, 1 do
+			highlight.highlight(M.mainwindowbuf,i)
+		end
+	end
+end
 function M.add_mappings()
 	--print(M.mainwindowbuf)
 	local opts = { nowait = true, noremap = true, silent = true }
@@ -45,6 +75,8 @@ function M.add_mappings()
 	vim.api.nvim_buf_set_keymap(M.mainwindowbuf, "n", "<leader>tf", ":lua require'csvtools'.NewWindow()<cr>", opts)
 	vim.api.nvim_buf_set_keymap(M.buf, "n", "<leader>td", ":lua require'csvtools'.CloseWindow()<cr>", opts)
 	vim.api.nvim_buf_set_keymap(M.mainwindowbuf, "n", "<leader>td", ":lua require'csvtools'.CloseWindow()<cr>", opts)
+	vim.api.nvim_buf_set_keymap(M.mainwindowbuf, "n", "<up>", ":lua require'csvtools'.Highlight()<cr>:-1<cr>", opts)
+	vim.api.nvim_buf_set_keymap(M.mainwindowbuf, "n", "<down>", ":lua require'csvtools'.Highlight()<cr>:+1<cr>", opts)
 end
 function M.setup(opts)
 	M = vim.tbl_deep_extend("force", M, opts)
